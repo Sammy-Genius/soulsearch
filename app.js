41719c03e752ad88ef148bd0c8de2bd31384cd5e
sammy-genius <mrsammadeit@gmail.com>
2020-07-20 01:47:51 +0000

    soulsearch code

---
+++ app.js
@@ -0,0 +1,37 @@
+const menuBtn = document.querySelector(".menu-btn");
+const sideMenu = document.querySelector(".side-menu");
+const logo = document.querySelector(".logo");
+const closeBtn = document.querySelector(".close-btn");
+let tl = gsap.timeline();
+
+tl.from("body", {opacity:0, duration:1, y:50, stagger:.5});
+
+//open the side menu
+
+menuBtn.addEventListener("click", () => {
+  if (sideMenu.style.visibility == "hidden") {
+      sideMenu.style.visibility = "visible";
+      sideMenu.style.width = "100%";
+      sideMenu.style.height = "100vh";
+      sideMenu.style.borderRadius = "0px 0px 300px 0px";
+      tl.from(".side-menu li", {opacity:0, y:50, stagger:.5});
+  } 
+  else {
+    sideMenu.style.visibility = "hidden";
+  }
+});
+
+//close the side menu 
+
+closeBtn.addEventListener("click", () => {
+  if (sideMenu.style.visibility == "visible") {
+      sideMenu.style.visibility = "hidden";
+      sideMenu.style.visibility = "visible";
+      sideMenu.style.width = "0%";
+      sideMenu.style.height = "0vh";
+      sideMenu.style.borderRadius = "50%";
+  } 
+  else {
+    sideMenu.style.visibility = "visible";
+  }
+});
