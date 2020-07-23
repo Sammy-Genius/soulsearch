ab60dae21f933a62117c5d30b7188b76456968ec
sammy-genius <mrsammadeit@gmail.com>
2020-07-23 11:58:46 +0000

    several additions to page

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ tl.from("body", {opacity:0, duration:1, y:50, stagger:.5}, "+=.3");
 menuBtn.addEventListener("click", () => {
   if (sideMenu.style.visibility == "hidden") {
       sideMenu.style.visibility = "visible";
-      sideMenu.style.width = "100%";
+      sideMenu.style.width = "80%";
       sideMenu.style.height = "100vh";
-      sideMenu.style.borderRadius = "0px 0px 300px 0px";
+      sideMenu.style.borderRadius = "0px 0px 0px 400px";
       tl.from(".side-menu .side-menu-list li", {opacity:0, y:50, stagger:.5});
   } 
   else {
